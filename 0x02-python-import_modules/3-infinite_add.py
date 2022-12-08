9d32962fc4bb689057736a77ce1268d1c08a8004
Akasaheed <Akasaheed@gmail.com>
2022-12-07 19:44:10 -0800

    import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
+    if len(sys.argv) == 1:
+        print(0)
+    elif len(sys.argv) == 2:
+        print(sys.argv[1])
+    else:
+        sum = 0
+        for i in range(1, len(sys.argv)):
+            a = int(sys.argv[i])
+            sum = sum + a
+        print(sum)
